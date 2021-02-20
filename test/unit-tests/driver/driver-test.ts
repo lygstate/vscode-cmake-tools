1bc13244f351c5c7b0c758ba3b0c11b616e75131
Yonggang Luo <luoyonggang@gmail.com>
2021-02-20 04:45:44 +0000

    Trying to use environment variable to specify visual studio.

--- test/unit-tests/driver/driver-test.ts
+++ test/unit-tests/driver/driver-test.ts
@@ -45,10 +45,7 @@ export function makeDriverTestsuite(driver_generator: (cmake: CMakeExecutable,
     let kitDefault: Kit;
     if (process.platform === 'win32') {
       kitDefault = {
-        name: 'Visual Studio Community 2017 - amd64',
-        visualStudio: 'VisualStudio.15.0',
-        visualStudioArchitecture: 'amd64',
-        preferredGenerator: {name: 'Visual Studio 15 2017', platform: 'x64'}
+        name: process.env.CMT_KIT_DEFAULT_NAME
       } as Kit;
     } else {
       kitDefault
@@ -58,9 +55,7 @@ export function makeDriverTestsuite(driver_generator: (cmake: CMakeExecutable,
     let kitNinja: Kit;
     if (process.platform === 'win32') {
       kitNinja = {
-        name: 'Visual Studio Community 2017 - amd64',
-        visualStudio: 'VisualStudio.15.0',
-        visualStudioArchitecture: 'amd64',
+        name: process.env.CMT_KIT_DEFAULT_NAME,
         preferredGenerator: {name: 'Ninja'}
       } as Kit;
     } else {
